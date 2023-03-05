8e70df0ef054f1605d5d715ae7db76b0f33dcd14
miika1006 <18673835+miika1006@users.noreply.github.com>
2023-03-05 12:31:08 +0200

    Added entity framework, dbcontext, and reference to infrastructure project

---
+++ src/Template.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Template.Core.Item;
+
+namespace Template.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
