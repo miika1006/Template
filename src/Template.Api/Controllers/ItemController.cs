bd081e1941a5e4e33482013b69d28b44b6600f1d
miika1006 <18673835+miika1006@users.noreply.github.com>
2023-03-22 10:10:55 +0200

    Added initial untested crud functionality

--- src/Template.Api/Controllers/ItemController.cs
+++ src/Template.Api/Controllers/ItemController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
 using Template.Core.Item;
 
 namespace Template.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+[Consumes(MediaTypeNames.Application.Json)]
+[Produces(MediaTypeNames.Application.Json)]
 public class ItemController : ControllerBase
 {
     private readonly ILogger<ItemController> _logger;
@@ -17,9 +20,52 @@ public class ItemController : ControllerBase
     }
 
     [HttpGet(Name = "GetItems")]
-    public async Task<ActionResult<IEnumerable<Item>>> GetItems()
+    [ProducesResponseType(typeof(List<Item>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<List<Item>>> GetItems()
     {
-        return await _itemRepository.QueryItemsAsync();
+        return Ok(await _itemRepository.QueryItemsAsync());
     }
+
+    [HttpGet("{id}",Name = "GetItem")]
+    [ProducesResponseType(typeof(Item),StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<List<Item>>> GetItem([FromRoute]long id)
+    {
+        var item = await _itemRepository.GetAsync(id);
+        if (item == null) return NotFound();
+        return Ok(item);
+    }
+
+    [HttpPost(Name = "CreateItem")]
+    [ProducesResponseType(typeof(Item), StatusCodes.Status201Created)]
+    public async Task<ActionResult<Item>> CreateItem([FromBody] Item item)
+    {
+        var created = _itemRepository.Add(item);
+        await _itemRepository.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetItem), new { id = created.Id });
+    }
+
+    [HttpPut("{id}", Name = "UpdateItem")]
+    [ProducesResponseType(typeof(Item), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Item>> UpdateItem([FromRoute] long id, [FromBody] Item item)
+    {
+        var updated = _itemRepository.Update(item);
+        if (item == null) return NotFound();
+        await _itemRepository.SaveChangesAsync();
+        return Ok(updated);
+    }
+
+    [HttpDelete("{id}", Name = "DeleteItem")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<List<Item>>> DeleteItem([FromRoute] long id)
+    {
+        var result = await _itemRepository.RemoveAsync(id);
+        if (result == false) return NotFound();
+        await _itemRepository.SaveChangesAsync();
+        return NoContent();
+    }
+
 }
 
