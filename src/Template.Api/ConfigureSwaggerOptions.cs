4b3359c12a4c75fc0ac12436cf72f32f99354fcc
miika1006 <18673835+miika1006@users.noreply.github.com>
2023-04-29 09:59:58 +0300

    Configured swagger with multiple version swaggerUi page

---
+++ src/Template.Api/ConfigureSwaggerOptions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Text;
+
+namespace Template.Api
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider provider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigureSwaggerOptions"/> class.
+        /// </summary>
+        /// <param name="provider">The <see cref="IApiVersionDescriptionProvider">provider</see> used to generate Swagger documents.</param>
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
+
+        /// <inheritdoc />
+        public void Configure(SwaggerGenOptions options)
+        {
+            // add a swagger document for each discovered API version
+            // note: you might choose to skip or document deprecated API versions differently
+            foreach (var description in provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+            }
+        }
+
+        private static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var text = new StringBuilder("An example application with OpenAPI, Swashbuckle, and API versioning.");
+            var info = new OpenApiInfo()
+            {
+                Title = "Example API",
+                Version = description.ApiVersion.ToString(),
+                Contact = new OpenApiContact() { Name = "Bill Mei", Email = "bill.mei@somewhere.com" },
+                License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+            };
+
+            info.Description = text.ToString();
+
+            return info;
+        }
+    }
+}
+
--- src/Template.Api/Program.cs
+++ src/Template.Api/Program.cs
@@ -1,14 +1,20 @@
 ﻿using Asp.Versioning;
 using Asp.Versioning.Conventions;
+using Asp.Versioning.ApiExplorer;
 using Microsoft.Extensions.DependencyInjection;
 using Template.Core.Item;
 using Template.Infrastructure.Data;
 using Template.Infrastructure.Data.Repositories;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Template.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 var databaseConnectionString = builder.Configuration.GetConnectionString("Database");
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 builder.Services.AddApiVersioning(
                     options =>
                     {
@@ -16,6 +22,7 @@ builder.Services.AddApiVersioning(
                         // "api-supported-versions" and "api-deprecated-versions"
                         options.ReportApiVersions = true;
                         options.ApiVersionReader = new UrlSegmentApiVersionReader();
+                        
                     })
                 .AddMvc(
                     options =>
@@ -24,16 +31,32 @@ builder.Services.AddApiVersioning(
                         // automatically applies an api version based on the name of
                         // the defining controller's namespace
                         options.Conventions.Add(new VersionByNamespaceConvention());
-                    });
+                    })
+                .AddApiExplorer(options =>
+                {
+                    options.GroupNameFormat = "'v'VVV";
+                    options.SubstituteApiVersionInUrl = true;
+                });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
     {
         policy.WithOrigins(builder.Configuration["CorsHosts"]?.Split(";") ?? Array.Empty<string>())
                .AllowAnyHeader().AllowAnyMethod().AllowCredentials();
     }));
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+builder.Services.AddSwaggerGen(options =>
+{
+
+    var fileName = typeof(Program).Assembly.GetName().Name + ".xml";
+    var filePath = Path.Combine(AppContext.BaseDirectory, fileName);
+
+    // integrate xml comments
+    options.IncludeXmlComments(filePath);
+});
 
 builder.Services.AddApplicationDbContext(databaseConnectionString);
 builder.Services.AddRepository<IItemRepository, ItemRepository>();
@@ -46,7 +69,18 @@ await app.CreateOrUpdateDatabaseAsync(databaseConnectionString, app.Logger);
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        var descriptions = app.DescribeApiVersions();
+
+        // build a swagger endpoint for each discovered API version
+        foreach (var description in descriptions)
+        {
+            var url = $"/swagger/{description.GroupName}/swagger.json";
+            var name = description.GroupName.ToUpperInvariant();
+            options.SwaggerEndpoint(url, name);
+        }
+    });
 }
 
 
