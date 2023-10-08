f48adb6c29002322cd803103f0e4c71d6f4ce51d
miika1006 <18673835+miika1006@users.noreply.github.com>
2023-10-08 13:34:43 +0300

    installed and tested package use-delayed-loading

--- react-web/src/src/pages/front/index.tsx
+++ react-web/src/src/pages/front/index.tsx
@@ -1,5 +1,6 @@
 import { FC, useState } from "react"; // we need this to make JSX compile
 import { textresources } from "../../common/resources";
+import { useDelayedLoading } from "use-delayed-loading";
 
 type FrontPageProps = {
 	resources: textresources;
@@ -7,12 +8,22 @@ type FrontPageProps = {
 
 export const FrontPage: FC<FrontPageProps> = ({ resources }) => {
 	const [count, setCount] = useState(0);
+	const [loading, setLoading] = useDelayedLoading(false);
 
+	const loadData = async () => {
+		setLoading(true);
+		const result = await fetch("https://dummyjson.com/products").then((res) =>
+			res.json()
+		);
+		setLoading(false);
+	};
 	return (
 		<div>
 			<h1>{resources.FRONTPAGE}</h1>
 			<p>{resources.format(resources.YOU_CLICKED, count)}</p>
 			<button onClick={() => setCount(count + 1)}>{resources.CLICK}</button>
+			<button onClick={() => loadData()}>Lataa</button>
+			{loading && <div>Ladataan...</div>}
 		</div>
 	);
 };
--- react-web/src/src/pages/second/index.tsx
+++ react-web/src/src/pages/second/index.tsx
@@ -1,14 +1,26 @@
-import React, { FC } from "react"; // we need this to make JSX compile
+import { FC } from "react"; // we need this to make JSX compile
 import { textresources } from "../../common/resources";
+import { useDelayedLoading } from "use-delayed-loading";
 
 type SecondPageProps = {
 	resources: textresources;
 };
 
 export const SecondPage: FC<SecondPageProps> = ({ resources }) => {
+	const [loading, setLoading] = useDelayedLoading(false, 1000);
+
+	const loadData = async () => {
+		setLoading(true);
+		const result = await fetch("https://dummyjson.com/products").then((res) =>
+			res.json()
+		);
+		setLoading(false);
+	};
 	return (
 		<div>
 			<h1>{resources.SECONDPAGE}</h1>
+			<button onClick={() => loadData()}>Lataa</button>
+			{loading && <div>Ladataan...</div>}
 		</div>
 	);
 };
